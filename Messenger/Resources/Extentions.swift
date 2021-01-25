6a4cb9e03924cd2f750aa4be599415b9dcfea637
jhchoo <jhchoo.kr@gmail.com>
2021-01-25 14:42:30 +0900

    Part2. 로그인 및 등록 화면 구성 완료.

---
+++ Messenger/Resources/Extentions.swift
@@ -0,0 +1,31 @@
+//
+//  Extentions.swift
+//  Messenger
+//
+//  Created by jae hwan choo on 2021/01/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width : CGFloat {
+        return self.frame.size.width
+    }
+    public var height : CGFloat {
+        return self.frame.size.height
+    }
+    public var top : CGFloat {
+        return self.frame.origin.y
+    }
+    public var bottom : CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    public var left : CGFloat {
+        return self.frame.origin.x
+    }
+    public var right : CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
+
