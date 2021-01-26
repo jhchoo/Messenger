af3331a56c9825cbfb9eb867f90230b6ce4fb94c
jhchoo <jhchoo.kr@gmail.com>
2021-01-26 14:15:33 +0900

    DatabaseManager file add

---
+++ Messenger/Resources/DatabaseManager.swift
@@ -0,0 +1,55 @@
+//
+//  DataBaseManager.swift
+//  Messenger
+//
+//  Created by jae hwan choo on 2021/01/26.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+}
+
+// MARK: 사용자 매니저
+extension DatabaseManager {
+    
+    public func userExists (with email: String, complition: @escaping((Bool) -> Void)) {
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value != nil else {
+                complition(false)
+                return
+            }
+            complition(true)
+        })
+
+    }
+    
+    /// Insert new users to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.safeEmail).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName,
+        ])
+    }
+}
+
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+}
